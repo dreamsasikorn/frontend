b67cffb8fac38d319e24b8ee25d18007ecd75c3d
sasikorn <sasikorn.k@jasmine.com>
2022-10-26 16:56:47 +0700

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+// import Container from '../node_modules/react-bootstrap/Container';
+import Navbar from '../node_modules/react-bootstrap/Navbar';
+// export default TextLinkExample;
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg="dark" variant="dark">
+        <p>EVALAUTE REPORT</p>
+
+      </Navbar>
     </div>
   );
 }
